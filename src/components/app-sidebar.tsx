ca6d43cda49c5b86e9ddf8f4b95221bcfe8a4f89
S.A.M <akhilfilms02@gmail.com>
2026-01-12 14:22:54 -0600

    fix logout button overflow

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -277,9 +277,9 @@ export function AppSidebar({
                 </SidebarMenuButton>
               </DropdownMenuTrigger>
               <DropdownMenuContent
-                className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
+                className="w-[--radix-dropdown-menu-trigger-width] min-w-28 z-100 rounded-lg"
                 side="top"
-                align="end"
+                align="start"
                 sideOffset={4}
               >
                 {/* <DropdownMenuSeparator /> */}
